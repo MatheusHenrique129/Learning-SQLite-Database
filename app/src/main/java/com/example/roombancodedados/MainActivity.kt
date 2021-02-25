0148a13f0ee5ad83ff025e07794fb497f6b4dfe3
Matheus Henrique <matheustennant@gmail.com>
2021-02-25 16:54:18 -0300

    Adicionando e listando todos os Contatos no Banco de Dados

--- app/src/main/java/com/example/roombancodedados/MainActivity.kt
+++ app/src/main/java/com/example/roombancodedados/MainActivity.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.room.Room
+import com.example.roombancodedados.dao.AppDataBase
+import com.example.roombancodedados.model.Contato
+import kotlinx.android.synthetic.main.activity_main.*
 
-    private lateinit var buttonNovoContato: ImageButton
+private lateinit var buttonNovoContato: ImageButton
     private lateinit var editNome: EditText
     private  lateinit var editTelefone: EditText
     private lateinit var  buttonSalvar: Button
     private lateinit var buttonCancelar: Button
     private lateinit var dialog: AlertDialog
+    private lateinit var txtLista: TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,18 +28,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         buttonNovoContato = findViewById(R.id.button_add_contato)
         buttonNovoContato.setOnClickListener(this)
-
-
-//        var contato = Contato(nomeContato =  "Matheus", telefoneContato = "4002-8517")
-//
-//        //Obtendo o banco, ou caso não exista o banco ele cria um!
-//        val db = Room.databaseBuilder(
-//            this,
-//            AppDataBase::class.java,
-//            "db_contato").allowMainThreadQueries().build()
-//
-//        val contatoDao = db.ContatoDao()
-//        contatoDao.salvar(contato)
+        txtLista = findViewById(R.id.text_lista)
+        txtLista.setOnClickListener(this)
 
     }
 
@@ -43,9 +39,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         abrirCadastroContato()
         } else if (v.id == R.id.button_cancelar){
             dialog.dismiss()
-        } else {
+        } else if (v.id == R.id.button_salvar) {
             salvarContato()
+        } else {
+            exibirContatos()
         }
+
+    }
+
+    private fun exibirContatos() {
+        val db = Room.databaseBuilder(
+            this,
+            AppDataBase::class.java,
+            "db_contato").allowMainThreadQueries().build()
+
+        val contatoDao = db.ContatoDao()
+
+        val contatos = contatoDao.listarTodos()
+        val separador = " - "
+
+        txtLista.text = ""
+
+        for (contato in contatos) {
+            txtLista.text = "${txtLista.text} $separador ${contato.nomeContato}"
+        }
+
+    }
+
+    private fun salvarContato() {
+        var contato = Contato(nomeContato = editNome.text.toString(), telefoneContato = editTelefone.text.toString())
+
+        //Obtendo o banco, ou caso não exista o banco ele cria um!
+        val db = Room.databaseBuilder(
+            this,
+            AppDataBase::class.java,
+            "db_contato").allowMainThreadQueries().build()
+
+        val contatoDao = db.ContatoDao()
+        contatoDao.salvar(contato)
 
     }
 
@@ -61,13 +92,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         buttonCancelar = view.findViewById(R.id.button_cancelar)
 
         buttonCancelar.setOnClickListener(this)
+        buttonSalvar.setOnClickListener(this)
 
         dialog = alertDialog.create()
         dialog.setCancelable(false)
         dialog.show()
 
         buttonCancelar.setOnClickListener(this)
-
 
 
         //alertDialog.setCancelable(false).create().show()
